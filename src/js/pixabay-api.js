9f30aca193d76a1fa46a5e1950d2a3e78e612471
Sergii Marchenko <marikmarchelo@gmail.com>
2024-07-05 12:56:37 +0300

    git add new commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,15 @@
+const API_KEY = '44791015-436dd02a5fc0b5187e9b97af9';
+const urlApi = 'https://pixabay.com/api/';
+
+export function fetchImages(query) {
+  const url = `${urlApi}?key=${API_KEY}&q=${query}&image_type=foto&orientation=horizontal&safesearch=true`;
+  fetch(url)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+      return res.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
-console.log('hello');
+import { fetchImages } from './js/pixabay-api';
+const buttonEl = document.querySelector('.button-js');
+const divEl = document.querySelector('.js-div');
+buttonEl.addEventListener('click', onClick);
+
+function onClick(e) {
+  e.preventDefault();
+  const query = document.querySelector('.search-js').value.trim();
+  fetchImages(query)
+    .then(images => randomMarcup(images))
+    .catch(error => console.log('error'));
+}
+
+function clearDivEl() {
+  divEl.innerHTML = '';
+}
+/////////////////////////////////////////////////////
+function randomMarcup(images) {
+  clearDivEl();
+
+  images.forEach(image => {
+    const gallery = `<div>
+    <img src="${image.webformatURL}" alt="${image.tags}" />
+    <div>
+      <p>${image.likes}</p>
+      <p>${image.views}</p>
+      <p>${image.comments}</p>
+      <p>${image.downloads}</p>
+    </div>
+  </div>`;
+    divEl.insertAdjacentHTML('beforeend', gallery);
+  });
+}
