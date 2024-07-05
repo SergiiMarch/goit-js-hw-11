12e85fb75fa545ba69191c92c29afbd7f0fac572
Sergii Marchenko <marikmarchelo@gmail.com>
2024-07-05 15:25:06 +0300

    git add commit

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,13 +3,16 @@ const urlApi = 'https://pixabay.com/api/';
 
 export function fetchImages(query) {
   const url = `${urlApi}?key=${API_KEY}&q=${query}&image_type=foto&orientation=horizontal&safesearch=true`;
-  fetch(url)
+  console.log('Fetching images from URL:', url);
+
+  return fetch(url)
     .then(res => {
       if (!res.ok) {
         throw new Error(res.status);
       }
+
       return res.json();
     })
-    .then(data => console.log(data))
+    .then(data => console.log(data.hits))
     .catch(error => console.log(error));
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { fetchImages } from './js/pixabay-api';
 const buttonEl = document.querySelector('.button-js');
 const divEl = document.querySelector('.js-div');
+
 buttonEl.addEventListener('click', onClick);
 
 function onClick(e) {
@@ -15,19 +16,26 @@ function clearDivEl() {
   divEl.innerHTML = '';
 }
 /////////////////////////////////////////////////////
+
 function randomMarcup(images) {
   clearDivEl();
 
   images.forEach(image => {
-    const gallery = `<div>
-    <img src="${image.webformatURL}" alt="${image.tags}" />
-    <div>
-      <p>${image.likes}</p>
-      <p>${image.views}</p>
-      <p>${image.comments}</p>
-      <p>${image.downloads}</p>
-    </div>
-  </div>`;
+    const gallery = `
+        <div class="card">
+          <div class="card-img-top">
+            <img src="${image.webformatURL}" alt="${image.tags}" width="360px">
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">${image.tags}</h5>
+            <p>Likes: ${image.likes}</p>
+            <p>Views: ${image.views}</p>
+            <p>Comments: ${image.comments}</p>
+            <p>Downloads: ${image.downloads}</p>
+          </div>
+        </div>`;
+
+    // Вставляємо картку в контейнер
     divEl.insertAdjacentHTML('beforeend', gallery);
   });
 }
